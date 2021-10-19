333a5d39f6c412dee199b489a1e7c182447b9730
FMGordillo <facundomgordillo@gmail.com>
2021-10-19 20:50:39 -0300

    Minor refactor

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,48 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 const Nav = styled.nav`
   background: hsl(40deg, 40%, 50%);
-
   ul {
     display: flex;
     align-items: left;
     list-style: none;
   }
-  ul > li {
-    margin: 14px;
+`;
+
+const StyledLink = styled(Link)`
+  color: hsl(40deg, 50%, 10%);
+  text-decoration: none;
+  transition: all 0.4s;
+  &:hover {
+    color: hsl(40deg, 40%, 80%);
   }
-  ul > li > a {
-    text-decoration: none;
+`;
+
+const Li = styled.li`
+  padding: 14px;
+  border-bottom: ${({ active }) =>
+    active ? "1px solid hsl(40deg, 80%, 70%)" : "none"};
+
+  background-color: ${({ active }) =>
+    active ? "hsl(40deg, 60%, 60%)" : "none"};
+
+  a {
   }
 `;
 
 const Menu = () => {
+  const location = useLocation();
+
   return (
     <Nav>
       <ul>
-        <li>
-          <Link to="/">Index</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
+        <StyledLink to="/">
+          <Li active={location.pathname === "/"}>Index</Li>
+        </StyledLink>
+        <StyledLink to="/users">
+          <Li active={location.pathname === "/users"}>Users</Li>
+        </StyledLink>
       </ul>
     </Nav>
   );
