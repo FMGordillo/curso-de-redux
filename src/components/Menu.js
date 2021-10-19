2be30078569cc0d0083ac4b91253b7a262922723
FMGordillo <facundomgordillo@gmail.com>
2021-10-19 20:35:53 -0300

    :sparkles: Minor refactor

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,34 @@
+import styled from "styled-components";
 import { Link } from "react-router-dom";
+
+const Nav = styled.nav`
+  background: hsl(40deg, 40%, 50%);
+
+  ul {
+    display: flex;
+    align-items: left;
+    list-style: none;
+  }
+  ul > li {
+    margin: 14px;
+  }
+  ul > li > a {
+    text-decoration: none;
+  }
+`;
 
 const Menu = () => {
   return (
-    <nav>
+    <Nav>
       <ul>
-        <li><Link to="/">Index</Link></li>
-        <li><Link to="/users">Users</Link></li>
+        <li>
+          <Link to="/">Index</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
       </ul>
-    </nav>
+    </Nav>
   );
 };
 
