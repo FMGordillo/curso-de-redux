bc8a8b71c18bb3fda6ce6d427fb98a3ebf7275e8
FMGordillo <facundomgordillo@gmail.com>
2022-06-25 09:36:38 +0200

    fix: removed unnecesary imports

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,14 +1,14 @@
-import { FunctionComponent } from "react";
+import { CSSProperties, FC } from "react";
 import icons from "./icons.module.css";
 
 type IconProps = {
-  style?: React.CSSProperties;
+  style?: CSSProperties;
 };
 
-export const Eye: FunctionComponent<IconProps> = ({ style }) => (
+export const Eye: FC<IconProps> = ({ style }) => (
   <div className={icons.eye} style={style} />
 );
 
-export const Loading: FunctionComponent<IconProps> = ({ style }) => (
+export const Loading: FC<IconProps> = ({ style }) => (
   <div className={icons.loading} style={style} />
 );
