6a9195003b350002f8e1c72d3b4fd3cce081102f
FMGordillo <facundomgordillo@gmail.com>
2022-07-30 00:12:48 +0200

    fix(lint): fix linting and deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 const IndexPage = () => {
   return (
     <div>
-      I've been trying Redux for the first time, feel free to check it out.
+      I&apos;ve been trying Redux for the first time, feel free to check it out.
       Also, the Redux Developer Tool is enabled, even in production!
     </div>
   );
